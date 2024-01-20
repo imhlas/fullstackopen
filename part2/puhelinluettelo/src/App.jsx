addbd7ffb018deb5cfa0721157c86272a021265d
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-01-20 17:02:30 +0200

    Exercises 2.6 - 2.7

---
+++ part2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook!`)
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    }
+
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+    </div>
+  )
+
+}
+
+export default App
