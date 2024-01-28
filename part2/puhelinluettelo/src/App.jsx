5dd78fac8aff4fb49fc37274be88056b4ac033f0
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-01-28 14:58:45 +0200

    Frontend shows backend errors

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -97,6 +97,12 @@ const App = () => {
             setNotificationMessage(null)
           }, 5000)
         })
+        .catch(error => {
+          setErrorMessage(error.response.data.error)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
     }
   }
 
