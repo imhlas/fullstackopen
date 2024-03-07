6e21311b58bbd56b0487d9a507b4104271d42065
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-03-07 16:34:22 +0200

    Exercise 4.1

---
+++ part4/blogilista/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  
+  const Blog = mongoose.model('Blog', blogSchema)
+  
+  app.use(cors())
+  app.use(express.json())
+  
+  app.get('/api/blogs', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+  module.exports = app
