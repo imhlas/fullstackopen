99010edf0c1f6db45876abb15f3c0d8253ad8392
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-03-08 18:00:32 +0200

    Exercises 4.4 - 4.5

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
   }
+
+  const maxLikes = Math.max(...blogs.map(b => b.likes));
+  const favorite = blogs.find(b => b.likes === maxLikes);
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
