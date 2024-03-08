315d1baf5249bbce74d4b70d22eb5349ededee55
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-03-08 11:21:08 +0200

    Exercise 4.2

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -13,34 +14,10 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connection to MongoDB:', error.message)
   })
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
-  
+
   app.use(cors())
   app.use(express.json())
   
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+  app.use('/api/blogs', blogsRouter)
 
   module.exports = app
